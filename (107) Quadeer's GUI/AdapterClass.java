8f774115b85afd653de6235afaebf31c72dda1ff
Mohammad Abdul Quadeer <105970109+m-a-quadeer@users.noreply.github.com>
2023-01-21 14:39:28 +0530

    Add files via upload

---
+++ (107) Quadeer's GUI/AdapterClass.java
@@ -0,0 +1,26 @@
+//Java applet program to implement Adapter classes
+
+import java.applet.Applet ;
+import java.awt.* ;
+import java.awt.event.* ;
+
+public class AdapterClass extends Applet{
+	Label l ;
+	
+	public void init(){
+		l = new Label ("Mouse Listener Demo") ;
+		l.setFont(new Font("",1,30)) ;
+		add(l) ;
+	
+		addMouseListener(new MouseAdapter()  {
+			public void mouseClicked(MouseEvent e){
+				l.setText("You clicked mouse") ;
+			}
+			public void mouseReleased(MouseEvent e){
+				l.setText("Mouse released") ;
+			}
+		});
+	}
+}
+
+//<applet code = "AdapterClass" width = "500" height = "500"></applet>
